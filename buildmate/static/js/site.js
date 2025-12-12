55aba9861c34cf3a3b9d35954775139630b0c938
Ruon90 <143106386+Ruon90@users.noreply.github.com>
2025-12-12 11:53:19 +0000

    Remove redundant root static folder and deprecated site.js shims

--- buildmate/static/js/site.js
+++
@@ -1,6 +0,0 @@
-// Deprecated: site.js was consolidated into script.js. Keep a tiny shim to avoid errors
-// if any legacy templates happen to include this file. The real logic lives in
-// buildmate/static/js/script.js.
-(function(){
-  try{ console.info('site.js is deprecated; using script.js for site-wide JS.'); }catch(err){}
-})();
--- staticfiles/js/site.js
+++
@@ -1,4 +0,0 @@
-// Deprecated: staticfiles site.js consolidated into script.js. Keep a shim to avoid errors.
-(function(){
-  try{ console.info('staticfiles site.js deprecated; script.js is canonical'); }catch(err){}
-})();
